98e2496372f5481495e0e95ae4269865e25f96d3
Gus <g.sosa1@lab.matcom.uh.cu>
2016-09-23 10:48:22 -0500

    Configurating data
    I wrote the models
    Migrations enabled

---
+++ BookManagerApp/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookManagerApp.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ BookManagerApp/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookManagerApp.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
